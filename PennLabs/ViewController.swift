50763ebe11d5ae89dd6c7f7e5a6dfd4a19348317
Matthew Riley <mgriley97@gmail.com>
2017-02-11 12:25:08 -0500

    initial commit

---
+++ PennLabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PennLabs
+//
+//  Created by Matthew Riley on 2017-02-11.
+//  Copyright © 2017 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PennLabsTests/PennLabsTests.swift
@@ -0,0 +1,36 @@
+//
+//  PennLabsTests.swift
+//  PennLabsTests
+//
+//  Created by Matthew Riley on 2017-02-11.
+//  Copyright © 2017 Matthew Riley. All rights reserved.
+//
+
+import XCTest
+@testable import PennLabs
+
+class PennLabsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
